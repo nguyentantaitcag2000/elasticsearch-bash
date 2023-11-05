6162a8caf0400672b8ae54108cc8dc592045028e
unknown <nguyentantaitcag2000@gmail.com>
2023-11-05 18:24:47 +0700

    update backup-db

--- backup-db.sh
+++ backup-db.sh
@@ -4,9 +4,11 @@
 source ~/bash/.env
 
 MY_PASS="${PASS_GPG}"
+USERNAME_DB="${USERNAME_DB}"
+PASSWORD_DB="${PASSWORD_DB}"
 
 backup_file=/home/website/backup/backup_$(date +%F.%H%M%S).sql.gz
-/usr/bin/mysqldump --login-path=local lazycodet | gzip > $backup_file
+/usr/bin/mysqldump -u"$USERNAME_DB" -p"$PASSWORD_DB" lazycodet | gzip > $backup_file
 /usr/bin/echo "$MY_PASS" | /usr/bin/gpg --batch --passphrase-fd 0 -c $backup_file
 #rm $backup_file
 /usr/bin/php /home/website/cron/upload-to-telegram.php $backup_file |& tee /home/website/cron/error.log 2>&1
