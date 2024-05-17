eec742fa333681115ced64f6bb2a41c3d201099b
Tai <nguyentantaitcag2000@gmail.com>
2024-05-17 17:17:38 +0700

    update

--- elasticsearch/helper.sh
+++ elasticsearch/helper.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Get the directory of this script
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 # Change working directory to the directory of this script
-cd "$(dirname "$0")"
+cd "$SCRIPT_DIR"
 
 echo "---------------------------------"
 echo "1. Snapshot"
@@ -15,23 +18,22 @@ echo "8. Check Shard"
 echo -n "Choose options: "
 read OPTION
 
-
 if [ $OPTION -eq 1 ]; then
-    bash snapshot/helper.sh
+    bash "$SCRIPT_DIR/snapshot/helper.sh"
 elif [ $OPTION -eq 2 ]; then
-    bash index/helper.sh
+    bash "$SCRIPT_DIR/index/helper.sh"
 elif [ $OPTION -eq 3 ]; then
-    bash mapping/helper.sh
+    bash "$SCRIPT_DIR/mapping/helper.sh"
 elif [ $OPTION -eq 4 ]; then
-    bash document/helper.sh
+    bash "$SCRIPT_DIR/document/helper.sh"
 elif [ $OPTION -eq 5 ]; then
-    bash alias/helper.sh
+    bash "$SCRIPT_DIR/alias/helper.sh"
 elif [ $OPTION -eq 6 ]; then
-    bash reindex.sh
+    bash "$SCRIPT_DIR/reindex.sh"
 elif [ $OPTION -eq 7 ]; then
-    bash sync/sync.sh
+    bash "$SCRIPT_DIR/sync/sync.sh"
 elif [ $OPTION -eq 8 ]; then
-    bash check-shard.sh
+    bash "$SCRIPT_DIR/check-shard.sh"
 else
     echo "Invalid option"
 fi
