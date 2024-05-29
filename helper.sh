f8b87057c0eb69512c3d472ca0ea88f402c0e951
Tai <nguyentantaitcag2000@gmail.com>
2024-05-29 21:35:13 +0700

    update helper.sh

--- helper.sh
+++ helper.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
+# Define ANSI color codes
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+reset=$(tput sgr0)
+# Get the path dir had opened the terminal
+CURRENT_DIR_OPENED_TERMINAL="$( cd "$(dirname "$0")" ; pwd -P )"
 # Change working directory to the directory of this script
 # Get the directory of this script
+OS=""
+
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    echo "Running on Linux"
+    OS="Linux"
     SCRIPT_DIR="$( dirname "$(realpath "$0")" )"
 elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
-    echo "Running on Windows"
+    OS="Windows"
     SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 else
     echo "Unsupported operating system"
     exit 1
 fi
+echo "Running on $OS - $SCRIPT_DIR"
 # Change working directory to the directory of this script
 cd "$SCRIPT_DIR"
+echo "$CURRENT_DIR_OPENED_TERMINAL"
 echo "---------------------------------"
 echo "1. Elasticsearch"
 echo "2. Check disk usage"
+echo "3. Open Git repository"
 echo -n "Choose options: "
 read OPTION
 
@@ -37,6 +48,28 @@ elif [ $OPTION -eq 2 ]; then
     echo "=========="
 
     bash system/check-disk-usage.sh GB
+elif [ $OPTION -eq 3 ]; then
+    
+    # Change directory to the specified directory
+    # if [[ "$OS" == "Windows" ]]; then
+    #     # Check if the path starts with a "/"
+    #     if [[ "${CURRENT_DIR_OPENED_TERMINAL:0:1}" == "/" ]]; then
+    #         # Remove the leading "/" character
+    #         CURRENT_DIR_OPENED_TERMINAL="${CURRENT_DIR_OPENED_TERMINAL:1}"
+    #         # Insert ":" after the drive letter
+    #         CURRENT_DIR_OPENED_TERMINAL="${CURRENT_DIR_OPENED_TERMINAL:0:1}:${CURRENT_DIR_OPENED_TERMINAL:1}"
+    #     fi
+    # fi
+    
+    #Working in (Linux & Window) [Git Bash, VS Code, CMD]
+    cd "$CURRENT_DIR_OPENED_TERMINAL"
+    URL="$(git config --get remote.origin.url)"
+    if [[ "$URL" == "" ]]; then
+        echo "${red}ERROR: You must specify the origin url of Git${reset}"
+        exit 1
+    fi
+    start $(git config --get remote.origin.url)
+    echo "${green}Open repository is successfully${reset}"
 else
     echo "Invalid option"
 fi
