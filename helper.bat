3362af120ece0f37bf95f4ad4673569668aa0733
nguyentantaitcag2000 <nguyentantaitcag2000@gmail.com>
2024-06-19 11:43:45 +0700

    update helper.bat

--- helper.bat
+++ helper.bat
@@ -1,1 +1,7 @@
+@echo off
 sh helper.sh
+REM Nếu như câu lệnh trên lỗi, thì sẽ chạy lênh "helper.sh", thường thì nếu như có cài Git Bash, nó sẽ chạy ở Git Bash
+IF ERRORLEVEL 1 (
+    echo "The sh command failed. Running helper.sh directly."
+    helper.sh
+)
