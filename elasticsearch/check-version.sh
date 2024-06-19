e6e139abdea89d4a160d193753f826c368c01bf0
nguyentantaitcag2000 <nguyentantaitcag2000@gmail.com>
2024-06-19 23:46:38 +0700

    add check version elasticsearch

--- elasticsearch/check-version.sh
+++ elasticsearch/check-version.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+source ../function.sh
+SCRIPT_DIR=$(get_document_root)
 
+echo "Running on $OS - $SCRIPT_DIR"
 # Nhập nội dung của file .env ở thư mục gốc
-source ~/bash/.env
+source "$SCRIPT_DIR/.env"
 
 HOST="${SERVER_ES}"
 CURL_USER="${USERNAME_ES}:${PASSWORD_ES}"
 
 # Lấy version của Elasticsearch
-VERSION=$(curl -s --user $CURL_USER -X GET "${HOST}" | jq -r '.version.number')
+VERSION=$(curl -s --user $CURL_USER -X GET "${HOST}" | "$SCRIPT_DIR/jq64" -r '.version.number')
 
 echo "Elasticsearch version: ${VERSION}"
--- elasticsearch/helper.sh
+++ elasticsearch/helper.sh
@@ -2,39 +2,40 @@
 
 # Change working directory to the directory of this script
 cd "$(dirname "$0")"
+while true; do
+    echo "---------------------------------"
+    echo "1. Snapshot"
+    echo "2. Index"
+    echo "3. Mapping"
+    echo "4. Document"
+    echo "5. Alias"
+    echo "6. Reindex"
+    echo "7. Sync"
+    echo "8. Check Shard"
+    echo "9. Check Version"
+    echo -n "Choose options: "
+    read OPTION
 
-echo "---------------------------------"
-echo "1. Snapshot"
-echo "2. Index"
-echo "3. Mapping"
-echo "4. Document"
-echo "5. Alias"
-echo "6. Reindex"
-echo "7. Sync"
-echo "8. Check Shard"
-echo "9. Check Version"
-echo -n "Choose options: "
-read OPTION
 
-
-if [ $OPTION -eq 1 ]; then
-    bash snapshot/helper.sh
-elif [ $OPTION -eq 2 ]; then
-    bash index/helper.sh
-elif [ $OPTION -eq 3 ]; then
-    bash mapping/helper.sh
-elif [ $OPTION -eq 4 ]; then
-    bash document/helper.sh
-elif [ $OPTION -eq 5 ]; then
-    bash alias/helper.sh
-elif [ $OPTION -eq 6 ]; then
-    bash reindex.sh
-elif [ $OPTION -eq 7 ]; then
-    bash sync/sync.sh
-elif [ $OPTION -eq 8 ]; then
-    bash check-shard.sh
-elif [ $OPTION -eq 9 ]; then
-    bash check-version.sh
-else
-    echo "Invalid option"
-fi
+    if [ $OPTION -eq 1 ]; then
+        bash snapshot/helper.sh
+    elif [ $OPTION -eq 2 ]; then
+        bash index/helper.sh
+    elif [ $OPTION -eq 3 ]; then
+        bash mapping/helper.sh
+    elif [ $OPTION -eq 4 ]; then
+        bash document/helper.sh
+    elif [ $OPTION -eq 5 ]; then
+        bash alias/helper.sh
+    elif [ $OPTION -eq 6 ]; then
+        bash reindex.sh
+    elif [ $OPTION -eq 7 ]; then
+        bash sync/sync.sh
+    elif [ $OPTION -eq 8 ]; then
+        bash check-shard.sh
+    elif [ $OPTION -eq 9 ]; then
+        bash check-version.sh
+    else
+        echo "Invalid option"
+    fi
+done
