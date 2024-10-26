fa60529c67582de479e1e9514bda6bcd5ad33701
nguyentantaitcag2000 <nguyentantaitcag2000@gmail.com>
2024-10-26 15:46:55 +0700

    update backup-db

--- backup-db.sh
+++ backup-db.sh
@@ -14,6 +14,6 @@ backup_file=/home/website/backup/backup_$(date +%F.%H%M%S).sql.gz
 # Source: https://stackoverflow.com/a/5075216
 /usr/bin/mysqldump -u"$USERNAME_DB" -p"$PASSWORD_DB" lazycodet --routines | gzip > $backup_file
 /usr/bin/echo "$MY_PASS" | /usr/bin/gpg --batch --passphrase-fd 0 -c $backup_file
-#rm $backup_file
 /usr/bin/php /home/website/cron/upload-to-telegram.php $backup_file |& tee /home/website/cron/error.log 2>&1
 rm $backup_file.gpg
+rm $backup_file
